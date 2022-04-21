b30e55d2b081a7c4323f70e0c3ede30c0d5caac4
EthanBennett52 <72234446+EthanBennett52@users.noreply.github.com>
2022-04-21 16:46:30 -0400

    Added basic health bar and ammo counter
    Does not work yet but theyre there

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,55 +3,51 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    [Export] public int speed = 300;
+	[Export] public int speed = 300;
 
-    private int maxHealth = 100;
-    protected int health;
-    ProgressBar healthBar;
+	private int maxHealth = 100;
+	protected int health;
 
-    public Vector2 velocity = new Vector2();
+	public Vector2 velocity = new Vector2();
 
-    public void takeDamage(int damage){
-        health = health - damage;
-        healthBar.Value = health;
-    }
+	public void takeDamage(int damage){
+		health = health - damage;
+	}
 
-    public void GetInput() {
-        //LookAt(GetGlobalMousePosition());
-        velocity = new Vector2();
+	public void GetInput() {
+		//LookAt(GetGlobalMousePosition());
+		velocity = new Vector2();
 
-        if (Input.IsActionPressed("right")){
-            velocity.x += 1;
-        }
+		if (Input.IsActionPressed("right")){
+			velocity.x += 1;
+		}
 
-        if (Input.IsActionPressed("left")) {
-            velocity.x -= 1;
-        }
+		if (Input.IsActionPressed("left")) {
+			velocity.x -= 1;
+		}
 
-        if (Input.IsActionPressed("up")){
-            velocity.y -= 1;
-        }
+		if (Input.IsActionPressed("up")){
+			velocity.y -= 1;
+		}
 
-        if (Input.IsActionPressed("down")){
-            velocity.y += 1;
-        }
-        
-        velocity = velocity.Normalized() * speed;
+		if (Input.IsActionPressed("down")){
+			velocity.y += 1;
+		}
+		
+		velocity = velocity.Normalized() * speed;
 
-    }
-    
-    public override void _Ready()
-    {
-        health = maxHealth;
-        healthBar = GetNode<ProgressBar>("HealthBar");
-        healthBar.Value = maxHealth;
-        //Temp
-        healthBar.Hide();
-    }
+	}
+	
+	public override void _Ready()
+	{
+		health = maxHealth;
+		//Temp
 
-    public override void _Process(float delta)
-    {
-        GetInput();
-        velocity = MoveAndSlide(velocity);
-    }
+	}
+
+	public override void _Process(float delta)
+	{
+		GetInput();
+		velocity = MoveAndSlide(velocity);
+	}
 }
