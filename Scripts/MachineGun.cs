d75dbb6f3a0eb416087ff4b8cd54de0a5189ef4d
EthanBennett52 <72234446+EthanBennett52@users.noreply.github.com>
2022-04-22 16:23:03 -0400

    Connected World to everything else

--- Scripts/MachineGun.cs
+++ Scripts/MachineGun.cs
@@ -3,16 +3,16 @@ using System;
 
 public class MachineGun : Gun
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        base._Ready();
-        fireRate = .1;
-        damage = 10;
-        maxLoadedCapacity = 20;
-        loaded = maxLoadedCapacity;
-        reloadTime = 3;
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		fireRate = .1;
+		damage = 10;
+		maxLoadedCapacity = 20;
+		loaded = maxLoadedCapacity;
+		reloadTime = 3;
+	}
 
 
 }
