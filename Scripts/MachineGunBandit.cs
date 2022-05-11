07fa03fc2598c24e1b469ac8f389a90e4f9c57e7
Isaiah <44537428+TheRedHerring22@users.noreply.github.com>
2022-05-10 23:16:13 -0400

    added random enemy spawns

--- Scripts/MachineGunBandit.cs
+++ Scripts/MachineGunBandit.cs
@@ -3,49 +3,49 @@ using System;
 
 public class MachineGunBandit : Bandit
 {
-    private Timer shooting;
+	private Timer shooting;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        base._Ready();
-        health = 100;
-        speed = 120;
-        weapon.damage = 10;
-        shooting = GetNode<Timer>("ShootFor");
-        shooting.Connect("timeout", this, "_OnTimeout");
-        weapon.soundEffectVolume(-20);
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		health = 100;
+		speed = 120;
+		weapon.damage = 10;
+		shooting = GetNode<Timer>("ShootFor");
+		shooting.Connect("timeout", this, "_OnTimeout");
+		weapon.soundEffectVolume(-20);
+	}
 
-    protected override void ApproachState(){
+	protected override void ApproachState(){
 		weapon.LookAt(player.Position);
 
 		if (shotTimer.IsStopped()){
 			weapon.fire();
 
-            if(shooting.IsStopped()){
-                shooting.Start();
-            }
+			if(shooting.IsStopped()){
+				shooting.Start();
+			}
 		}
 
 		velocity = Position.DirectionTo(player.Position) * speed;
 		MoveAndSlide(velocity);
 	}
 
-    protected override void InPositionState(){
+	protected override void InPositionState(){
 		weapon.LookAt(player.Position);
 		if (shotTimer.IsStopped()){
 			weapon.fire();
 
 			if(shooting.IsStopped()){
-                shooting.Start();
-            }
+				shooting.Start();
+			}
 		}
 	}
 
-    private void _OnTimeout(){
-        shotTimer.Start();
-    }
+	private void _OnTimeout(){
+		shotTimer.Start();
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
--- Scripts/RifleBandit.cs
+++ Scripts/RifleBandit.cs
@@ -3,20 +3,20 @@ using System;
 
 public class RifleBandit : Bandit
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        base._Ready();
-        health = 50;
-        speed = 100;
-        weapon.damage = 50;
-        ((Rifle)weapon).penetrate = false;
-        weapon.soundEffectVolume(-10);
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		health = 50;
+		speed = 100;
+		weapon.damage = 50;
+		((Rifle)weapon).penetrate = false;
+		weapon.soundEffectVolume(-10);
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
